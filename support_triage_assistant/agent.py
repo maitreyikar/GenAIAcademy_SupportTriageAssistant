63fac0814114079525748000609994d831439afe
Maitreyi Kar <maitreyikar13@gmail.com>
2026-03-31 13:03:21 +0000

    update agent

--- support_triage_assistant/agent.py
+++ support_triage_assistant/agent.py
@@ -1,5 +1,15 @@
 import json
 from google.adk.agents import SequentialAgent, Agent
+from google.adk.tools.tool_context import ToolContext
+
+
+
+def add_prompt_to_state(
+    tool_context: ToolContext, prompt: str
+) -> dict[str, str]:
+    """Saves the user's initial prompt to the state."""
+    tool_context.state["PROMPT"] = prompt
+    return {"status": "success"}
 
 
 SUPPORT_CATEGORIES = {
@@ -14,29 +24,36 @@ SUPPORT_CATEGORIES = {
 
 
 CATEGORIZER_INSTRUCTION = f"""
-Analyze the user support message and identify the category.
+Analyze the user's PROMPT and identify the category.
 Output ONLY a JSON object in this format: {{"category": "label"}}
 Choose from: {list(SUPPORT_CATEGORIES.keys())}
+
+PROMPT:
+    {{ PROMPT }}
 """
 
 categorization_agent = Agent(
     model='gemini-2.5-flash',
     name='categorizer',
     description='Identifies the intent category of the message.',
-    instruction=CATEGORIZER_INSTRUCTION
+    instruction=CATEGORIZER_INSTRUCTION,
+    output_key="CATEGORY"
 )
 
 
-PRIORITY_INSTRUCTION = """
-Review the user message and the category assigned by the previous agent.
-Assess the priority: 'low', 'medium', 'high', or 'urgent'.
+PRIORITY_INSTRUCTION = f"""
+Review the user's PROMPT and the CATEGORY assigned by the previous agent.
+Assess the priority of the PROMPT: 'low', 'medium', 'high', or 'urgent'.
 - 'urgent': Outages, security, or legal threats.
 - 'high': Broken core features or high frustration.
 - 'medium': General bugs or questions with workarounds.
 - 'low': General feedback or feature requests.
 
 Output ONLY a JSON object containing both the existing category and the new priority.
-Format: {"category": "label", "priority": "level"}
+Format: {{"category": CATEGORY, "priority": "level"}}
+
+CATEGORY:
+    {{ CATEGORY }}
 """
 
 priority_agent = Agent(
@@ -47,8 +64,20 @@ priority_agent = Agent(
 )
 
 
-root_agent = SequentialAgent(
+triage_workflow = SequentialAgent(
     sub_agents=[categorization_agent, priority_agent],
-    name='smart_support_triage_pipeline'
+    name='triage_workflow',
+)
+
+root_agent = Agent(
+    name="greeter",
+    model='gemini-2.5-flash',
+    description="The main entry point for the support triage assistant",
+    instruction="""
+   When the user responds, use the 'add_prompt_to_state' tool to save their response.
+    After using the tool, transfer control to the 'triage_workflow' agent.
+    """,
+    tools=[add_prompt_to_state],
+    sub_agents=[triage_workflow]
 )
 
